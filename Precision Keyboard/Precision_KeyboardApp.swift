aad78f454c9c1e7eee0b99963d8f3d5d084037a9
Ninad Patil <ninadpatil1452@gmail.com>
2025-09-08 04:49:48 -0400

    Initial Commit

---
+++ Precision Keyboard/Precision_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Precision_KeyboardApp.swift
+//  Precision Keyboard
+//
+//  Created by Ninad Patil on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Precision_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Precision KeyboardTests/Precision_KeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Precision_KeyboardTests.swift
+//  Precision KeyboardTests
+//
+//  Created by Ninad Patil on 08/09/25.
+//
+
+import Testing
+@testable import Precision_Keyboard
+
+struct Precision_KeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
